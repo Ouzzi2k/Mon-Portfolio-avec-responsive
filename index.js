999111a192833c584fb9c05d1f2b32fa76d1f533
Ouzzi2k <128175087+Ouzzi2k@users.noreply.github.com>
2023-04-12 16:23:30 +0000

    Add files via upload
    C'est la nouvelle transformation de mon Portfolio, mais cette fois avec la responsivité.

---
+++ index.js
@@ -0,0 +1,10 @@
+const menuButton = document.querySelector('.bar');
+const menuList = document.querySelector('nav ul');
+
+menuButton.addEventListener('click', () => {
+  menuList.classList.toggle('show-menu');
+});
+
+
+
+
